27f3976cacbbaae38c861efd45864671f7ebf58b
Khoirul Asfian <mk.asfian@gmail.com>
2023-06-26 14:07:41 +0700

    feat: add error handler string fn & ts alias

--- src/middleware/validator.middleware.ts
+++ src/middleware/validator.middleware.ts
@@ -1,6 +1,7 @@
 import type { ValidationTargets } from "hono";
 import { validator } from "hono/validator";
 import { ZodSchema } from "zod";
+import { getErrorMessage } from "~/utils/errorHandler";
 
 export function validatorMiddleware(
 	target: keyof ValidationTargets,
@@ -9,6 +10,7 @@ export function validatorMiddleware(
 	return validator(target, (value, c) => {
 		const parsed = schema.safeParse(value);
 		if (!parsed.success) {
+			console.log("error: ", getErrorMessage(parsed.error));
 			return c.json({ message: "Invalid body!", error: parsed.error }, 400);
 		}
 		return parsed.data;
